0181b586a7d16572101c7bd9e8746fc66bd34cc0
alezniki <alezniki@gmail.com>
2017-04-22 17:39:31 +0200

    Uradjena klasa Artikal fixes #3

--- src/main/java/model/Stavka.java
+++ src/main/java/model/Stavka.java
@@ -1,7 +1,70 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by dzoni on 4/22/2017.
  */
+
+@DatabaseTable(tableName = "stavka")
 public class Stavka {
+
+    public static final String POLJE_KOLICINA = "kolicina";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_KOLICINA, canBeNull = false)
+    private int kolicina;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, foreignColumnName = "racun_id")
+    private Racun racun;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, foreignColumnName = "artikal_id")
+    private Artikal artikal;
+
+    public Stavka(){
+
+    }
+
+    public Stavka(int kolicina, Racun racun, Artikal artikal) {
+        this.kolicina = kolicina;
+        this.racun = racun;
+        this.artikal = artikal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getKolicina() {
+        return kolicina;
+    }
+
+    public void setKolicina(int kolicina) {
+        this.kolicina = kolicina;
+    }
+
+    public Racun getRacun() {
+        return racun;
+    }
+
+    public void setRacun(Racun racun) {
+        this.racun = racun;
+    }
+
+    public Artikal getArtikal() {
+        return artikal;
+    }
+
+    public void setArtikal(Artikal artikal) {
+        this.artikal = artikal;
+    }
+
+    @Override
+    public String toString() {
+        return "STAVKA: " + "ID: " + id + ", KOLICINA: " + kolicina;
+    }
 }
